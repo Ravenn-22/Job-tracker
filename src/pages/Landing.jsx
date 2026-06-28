bc1a24f3dd98a65ce819b3ef2730aa70fdc361b0
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-06-28 08:14:06 -0700

    Fix hook filename casing to match imports

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,7 +6,7 @@ import FadeIn from "../components/shared/FadeIn.jsx";
 import careerProgress from "../assets/career-progress.png";
 import workingLate from "../assets/working-late.png";
 import remoteWorker from "../assets/remote-worker.png";
-import useActiveSection from "../hooks/UseActiveSection.js";
+import useActiveSection from "../hooks/UseActSection.js";
 import skylineImg from "../assets/footer-skyline.jpg";
 
 const quickLinks = [
