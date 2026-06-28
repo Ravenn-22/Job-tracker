5279025428602245a7ae55fea9490673c8ccc419
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-06-28 09:21:06 -0700

    fixed axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://georn-tracker.onrender.com",
+  baseURL: "https://georn-tracker.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
